9624fe8bcce1de6edb858dde25a9e0f1a27f9dcb
Mohsin Asif <32785090+Mohsin-Asif@users.noreply.github.com>
2022-08-31 13:22:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import requests
 import pandas
 import snowflake.connector
-from urllib.error imoprt URLError
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
